149401fc1bf022af495cbb2ccd4c5a984c6157f0
bhuvalakshit29 <bhuvalaxit29@gmail.com>
2024-01-30 16:05:59 +0530

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from "../components/UI/common-section/CommonSection";
+
+export default function Contact() {
+    return (
+        <div>
+            <Helmet title="Contact">
+                <CommonSection title={'Contact'} />
+            </Helmet>
+        </div>
+    )
+}
